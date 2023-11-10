cdec70dc6bd53e48dd30a5ba7fb7695bef258455
Rajkumar <b-rajkumar@users.noreply.github.com>
2023-11-10 12:44:45 +0530

    Adds Dimension invalidation in Square create

--- src/main/com/tw/step/shapes/models/Square.java
+++ src/main/com/tw/step/shapes/models/Square.java
@@ -1,10 +1,14 @@
 package com.tw.step.shapes.models;
 
+import com.tw.step.shapes.exceptions.InvalidDimensionsException;
+
 public class Square {
   private final double side;
 
-  public static Square create(double side) {
-    return new Square(side);
+  public static Square create(double side) throws InvalidDimensionsException {
+    if (side >= 0) return new Square(side);
+
+    throw new InvalidDimensionsException(side, side);
   }
 
   public Square(double side) {
