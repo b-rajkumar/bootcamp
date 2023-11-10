8fd69e359828f101fe8917d2e60b9f41380396be
Rajkumar <b-rajkumar@users.noreply.github.com>
2023-11-10 18:51:18 +0530

    Adds method to calculate totalOutcome of events for a coin

--- src/main/com/tw/step/math/models/Coin.java
+++ src/main/com/tw/step/math/models/Coin.java
@@ -3,17 +3,20 @@ package com.tw.step.math.models;
 import com.tw.step.math.exceptions.InvalidRange;
 
 public class Coin {
+  private static double calculateTotalOutcomes(int coinsCount) {
+    return Math.pow(2, coinsCount);
+  }
 
   public static Chance chanceOfGettingAllTails(int coinsCount) throws InvalidRange {
-    double totalPossibilities = Math.pow(2, coinsCount);
-    double probability = 1 / totalPossibilities;
+    double totalOutcomes = calculateTotalOutcomes(coinsCount);
+    double probability = 1 / totalOutcomes;
 
     return Chance.initiate(probability);
   }
 
   public static Chance chanceOfGettingAtLeastOneTail(int coinsCount) throws InvalidRange {
-    double totalPossibilities = Math.pow(2, coinsCount);
-    double probability = (totalPossibilities - 1) / totalPossibilities;
+    double totalOutcomes = calculateTotalOutcomes(coinsCount);
+    double probability = (totalOutcomes - 1) / totalOutcomes;
 
     return Chance.initiate(probability);
   }
