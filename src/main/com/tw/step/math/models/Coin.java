fc1cfccce56d4535275cfb299507aacb2a08fff8
Rajkumar <b-rajkumar@users.noreply.github.com>
2023-11-10 19:30:46 +0530

    Update Coin class

--- src/main/com/tw/step/math/models/Coin.java
+++ src/main/com/tw/step/math/models/Coin.java
@@ -3,20 +3,17 @@ package com.tw.step.math.models;
 import com.tw.step.math.exceptions.InvalidRange;
 
 public class Coin {
-  private static double calculateTotalOutcomes(int coinsCount) {
-    return Math.pow(2, coinsCount);
-  }
 
   public static Chance chanceOfGettingAllTails(int coinsCount) throws InvalidRange {
-    double totalOutcomes = calculateTotalOutcomes(coinsCount);
-    double probability = 1 / totalOutcomes;
+    double totalPossibilities = Math.pow(2, coinsCount);
+    double probability = 1 / totalPossibilities;
 
     return Chance.initiate(probability);
   }
 
   public static Chance chanceOfGettingAtLeastOneTail(int coinsCount) throws InvalidRange {
-    double totalOutcomes = calculateTotalOutcomes(coinsCount);
-    double probability = (totalOutcomes - 1) / totalOutcomes;
+    double totalPossibilities = Math.pow(2, coinsCount);
+    double probability = (totalPossibilities - 1) / totalPossibilities;
 
     return Chance.initiate(probability);
   }
